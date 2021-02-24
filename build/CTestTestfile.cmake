933f69037c818448b314599288eec4f325d5a919
jason <freedom343@163.com>
2021-02-24 22:45:34 +0200

    first edit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/jason/catkin_ws/src
+# Build directory: /home/jason/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(teleop_twist_keyboard)
+subdirs(beginner_tutorials)
+subdirs(eth_ros_ex1)
